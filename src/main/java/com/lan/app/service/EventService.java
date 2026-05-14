c8a916c4f5083b84c867722f31e71b4106dc06a2
Misha <kokoulin92@gmail.com>
2026-05-14 12:44:19 +0400

    wip

--- src/main/java/com/lan/app/service/EventService.java
+++ src/main/java/com/lan/app/service/EventService.java
@@ -6,17 +6,19 @@ import java.util.UUID;
 import com.lan.app.domain.model.Event;
 import com.lan.app.repository.EventRepository;
 
+import io.quarkus.cache.CacheResult;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class EventService {
-    
+
     EventRepository repo;
 
     public EventService(EventRepository repo) {
         this.repo = repo;
     }
 
+    @CacheResult(cacheName = "events")
     public List<Event> list() {
         return repo.list();
     }
